7115fd09989593e58798dc9b4efe8e76b3e12985
Evan Summers <evan.summers@gmail.com>
2017-01-01 14:50:31 +0000

    initial

--- index.js
+++ index.js
@@ -255,7 +255,7 @@ function renderPage(ctx, content) {
     const botUrl = /(Mobile)/.test(ctx.get('user-agent'))
     ? `tg://${config.bot}`
     : `https://web.telegram.org/#/im?p=@${config.bot}`;
-    const botLink = `<a href="${botUrl}">${botUrl}</a>`;
+    const botLink = `<a href="${botUrl}">telegram.me/@${config.bot}</a>`;
     const paragraphs = content.paragraphs.map(p => p.replace('{botLink}', botLink));
     ctx.body = lodash.flatten([
         `<html>`,
